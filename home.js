f6934a521afd861d1703b2e1d6fdbb0075cddd62
Akhter-Hussain97 <akhterhussaindeveloper@gmail.com>
2025-03-28 23:52:53 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,17 @@
+const khp=document.getElementById("khaplu");
+let url="https://www.serenahotels.com/khaplu-palace?srsltid=AfmBOoojx8H-QULX4XmWAXFPiETWEF7muarHqgpUk9BZIboIE0-avjAN";
+let url1="https://www.booking.com/searchresults.en-gb.html?aid=356980&label=gog235jc-1DCAsotQFCHG1vdW50YWluLWxvZGdlLXNrYXJkdS1za2FyZHVICVgDaLUBiAEBmAEJuAEXyAEP2AED6AEB-AECiAIBqAIDuAKz1Zm_BsACAdICJGE3ZTc1NDU2LTY2YzYtNGFlNy1iMjMxLTFlY2E4ZmEwMmU5M9gCBOACAQ&highlighted_hotels=5886609&redirected=1&city=-2774916&hlrd=user_sh&source=hotel&expand_sb=1&keep_landing=1&sid=69335732bc7ad72cdb4b7c703219689c";
+khp.addEventListener('click',ReadKhp=()=>{
+    window.open(url,"_blank");
+})
+const skd=document.getElementById("skardu");
+skd.addEventListener('click',ReadSkd=()=>{
+     window.open(url1,"_blank");
+});
+const hza=document.getElementById("hunza");
+hza.addEventListener('click',ReadHza=()=>{
+     window.open(url1,"_blank");
+});
+
+
+  
